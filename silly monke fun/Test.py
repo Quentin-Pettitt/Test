5a0d661183838443de30dd3f170b235e832f4281
Quentin-Pettitt <159151875+Quentin-Pettitt@users.noreply.github.com>
2024-02-17 13:57:05 -0500

    Update Test.py

--- silly monke fun/Test.py
+++ silly monke fun/Test.py
@@ -1,1 +1,9 @@
 
+<!DOCTYPE html>
+<title>My First Website</title>
+</head>
+   <h1>HELLO WORLD</h1>
+   <h2> Coded By Quentin</h2>
+ <h1 style="color:blue;">A Blue Heading</h1>
+
+<h2 style="color:red;">A red paragraph.</p>
